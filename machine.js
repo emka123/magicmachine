efdf7f20d576b413f65391a2522b14ec3f111ac7
madzik <magdalena.pal90@gmail.com>
2016-01-30 15:51:02 +0100

    Checked source js

--- machine.js
+++ machine.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $("#quote-button").click(function(){
         $.ajax({
-            url: 'http://www.stands4.com/services/v2/quotes.php?uid=4596&tokenid=CFFBMw71bnIlF7fe',
+            url: 'https://www.stands4.com/services/v2/quotes.php?uid=4596&tokenid=CFFBMw71bnIlF7fe',
             success: function(date) {
                 quoteObject = date.querySelector('quote').firstChild
                 quoteString = new XMLSerializer().serializeToString(quoteObject)
